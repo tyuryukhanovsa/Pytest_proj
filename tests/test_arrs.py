4fd819b576fe3496ce020e1fa20a8c758b9d46b9
Sergey <serega.tiuriuhanov94@gmail.com>
2023-06-16 14:55:55 +0300

    added tests 100% on main code

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -5,11 +5,14 @@ from utils import arrs
 
 def test_get():
     assert arrs.get([1, 2, 3], 2, "test") == 3
-    # assert arrs.get([], 0, "test") == "test"
-    with pytest.raises(Exception):
-        arrs.get([], 0, "test")
+    assert arrs.get([1, 2, 3], 10, "test") == "test"
+    with pytest.raises(IndexError):
+         arrs.get([], 0, "test")
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([], 1) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5], -4) == [2, 3, 4, 5]
+    assert arrs.my_slice([1, 2, 3, 4, 5], -6) == [1, 2, 3, 4, 5]
